6098f71f008e832e92a0bbd5025cd522ccca2fa5
obakir <wldkng14@yahoo.com>
2015-02-08 18:44:26 -0500

    Create MusicPlayer.java

---
+++ MusicPlayer.java
@@ -0,0 +1,15 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextField;
+
+
+public class MusicPlayer extends JFrame {
+	JButton add, edit, delete, confirm, cancel;
+	JTextField name, artist, album, year;
+	JList songList;
+	
+	public MusicPlayer() {
+		
+	}
+}
