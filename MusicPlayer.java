5cfe908f1967b64fe6c8308038d310320e7554ec
tolbyflew <tolbyflew@gmail.com>
2015-02-08 18:46:45 -0500

    Update MusicPlayer.java
    Set Dimension

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -4,12 +4,23 @@ import javax.swing.JList;
 import javax.swing.JTextField;
 
 
-public class MusicPlayer extends JFrame {
+public class MusicPlayer extends JFrame{
+
 	JButton add, edit, delete, confirm, cancel;
 	JTextField name, artist, album, year;
 	JList songList;
 	
-	public MusicPlayer() {
+	public MusicPlayer(){
+		
+		
+	}
+	
+	public static void main(String[] args){
+		MusicPlayer mp = new MusicPlayer();
+		mp.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mp.setSize(300,400);
+		mp.setLocationRelativeTo(null);
+		mp.setVisible(true);
 		
 	}
 }
