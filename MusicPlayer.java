f9939e7668a79f1c97c7f802766b67ed9fe83a56
tolbyflew <tolbyflew@gmail.com>
2015-02-08 19:40:58 -0500

    Update MusicPlayer.java
    GUI Finalized

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -3,14 +3,18 @@ import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 
 
 public class MusicPlayer extends JFrame{
 
 	JButton add, edit, delete, confirm, cancel;
+	JLabel nLabel, artLabel, albLabel, yLabel;
 	JTextField name, artist, album, year;
 	JList songList;
 	
@@ -23,7 +27,19 @@ public class MusicPlayer extends JFrame{
 
 		// Set up the Panels
 		JPanel listPanel = new JPanel(new GridLayout(0,1));
-		JPanel buttonPanel = new JPanel(new GridLayout(0, 1));
+		JPanel buttonPanel = new JPanel(new GridLayout(0, 1, 0, 15 ));
+		JPanel textPanel = new JPanel(new GridLayout(0, 1, 0, 0));
+		
+		//AddListbox
+		songList = new JList();
+		songList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		songList.setSelectedIndex(0);
+		//songList.addListSelectionListener(this);
+		songList.setVisibleRowCount(15);
+	    JScrollPane listScrollPane = new JScrollPane(songList);
+		listPanel.add(listScrollPane);
+	    
+		//Add Buttons
 		add = new JButton("Add");
 		buttonPanel.add(add);
 		edit = new JButton("Edit");
@@ -35,18 +51,36 @@ public class MusicPlayer extends JFrame{
 		cancel = new JButton("Cancel");
 		buttonPanel.add(cancel);
 		
-		JPanel textPanel = new JPanel(new GridLayout(0, 1));
+		//Add TextFields
+		name = new JTextField("Name", 10);
+		nLabel = new JLabel("Name");
+		textPanel.add(nLabel);		
+		textPanel.add(name);
+		artLabel = new JLabel("Artist");
+		artist = new JTextField("Artist", 10);
+		textPanel.add(artLabel);
+		textPanel.add(artist);
+		albLabel = new JLabel("Album");
+		album = new JTextField("Album", 10);
+		textPanel.add(albLabel);
+		textPanel.add(album);
+		yLabel = new JLabel("Year");
+		year = new JTextField("Year", 10);
+		textPanel.add(yLabel);
+		textPanel.add(year);
 		
+		
+		//add the panels
 		add(listPanel);
-		add(buttonPanel);
 		add(textPanel);
-		
+		add(buttonPanel);
 	}
 	
 	public static void main(String[] args){
 		MusicPlayer mp = new MusicPlayer("Our Library");
 		mp.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		mp.setSize(300,400);
+		mp.setSize(550,300);
+		mp.setResizable(false);
 		mp.setLocationRelativeTo(null);
 		mp.setVisible(true);
 		
